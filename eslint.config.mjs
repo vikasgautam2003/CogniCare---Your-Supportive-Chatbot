de8a174c05bdfda25e24977a691031924e050a2d
Vikas Gautam <vikasgautamug2003@gmail.com>
2025-08-22 22:22:10 +0530

    vercel fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,32 @@
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+//   {
+//     ignores: [
+//       "node_modules/**",
+//       ".next/**",
+//       "out/**",
+//       "build/**",
+//       "next-env.d.ts",
+//     ],
+//   },
+// ];
+
+// export default eslintConfig;
+
+
+
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -19,6 +48,11 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+  },
+  {
+    rules: {
+      "react/no-unescaped-entities": "off", // ✅ allow normal English quotes/apostrophes
+    },
   },
 ];
 
