f4bcc00ef4589a585ae1e7a1c429dfa726fcb29d
Vikas Gautam <vikasgautamug2003@gmail.com>
2025-08-22 22:19:09 +0530

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   // Add any Next.js configuration options you need here.
   // For example:
   // reactStrictMode: true,
+  images: {
+    domains: ["placehold.co"], // ✅ allow external placeholder images
+  },
 };
 
 export default nextConfig;
