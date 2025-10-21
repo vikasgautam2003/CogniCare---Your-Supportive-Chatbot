c12f69d768951d58158b9abf9cf3b8bdc00739a7
Vikas Gautam <vikasgautamug2003@gmail.com>
2025-10-22 02:01:42 +0530

    fixes

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,6 @@
-const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
 
-export default config;
+module.exports = {
+  plugins: [tailwindcss, autoprefixer],
+};
